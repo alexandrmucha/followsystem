997118924a8f210757904bf7bf2c020d354cb4e6
Alexandr Mucha <130845337+alexandrmucha@users.noreply.github.com>
2026-05-14 19:16:46 +0200

    Added contacts page

---
+++ app/pages/contacts.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <!-- Title -->
+    <h1 class="text-xl font-semibold text-neutral-900 dark:text-neutral-100">
+      {{ t('contacts.title') }}
+    </h1>
+
+
+    
+  </div>
+</template>
+
+<script lang="ts" setup>
+const { t } = useI18n()
+
+useHead({
+  title: t('contacts.title')
+})
+
+definePageMeta({
+  layout: 'dashboard',
+})
+
+</script>
