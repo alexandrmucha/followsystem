555c2f617f44f8bff5cdcab261f9b808902d834c
Alexandr Mucha <130845337+alexandrmucha@users.noreply.github.com>
2026-05-16 14:07:09 +0200

    Added different google account fix

--- app/pages/gmail/callback.vue
+++ app/pages/gmail/callback.vue
@@ -28,16 +28,25 @@ onMounted(async () => {
   } catch (err: any) {
     let backendError: string | null = null
 
+    // 🟢 read backend error from Supabase HttpError
     if (err instanceof FunctionsHttpError) {
       const body = await err.context.json()
       backendError = body.error
     }
+
+    // 🔥 SPECIFIC ERRORS
 
     if (backendError === 'GMAIL_ALREADY_CONNECTED') {
       navigateTo("/settings?error=gmail_already_connected")
       return
     }
 
+    if (backendError === 'DIFFERENT_GOOGLE_ACCOUNT') {
+      navigateTo("/settings?error=different_google_account")
+      return
+    }
+
+    // 🔴 fallback
     navigateTo("/settings?error=gmail_general")
   }
 })
