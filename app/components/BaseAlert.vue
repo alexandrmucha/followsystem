86bc5676d5d906cfa54fa195e667c1233e11e734
Alexandr Mucha <130845337+alexandrmucha@users.noreply.github.com>
2026-05-19 00:53:07 +0200

    Fixed translation

--- app/components/BaseAlert.vue
+++ app/components/BaseAlert.vue
@@ -23,7 +23,7 @@
     </div>
 
     <!-- Close button -->
-    <button v-if="dismissible" type="button" class="ml-4 text-neutral-500 hover:text-neutral-700 dark:text-neutral-400 dark:hover:text-neutral-200 cursor-pointer" @click="visible = false" :title="$t('close')" :aria-label="$t('close')">
+    <button v-if="dismissible" type="button" class="ml-4 text-neutral-500 hover:text-neutral-700 dark:text-neutral-400 dark:hover:text-neutral-200 cursor-pointer" @click="visible = false" :title="$t('common.close')" :aria-label="$t('common.close')">
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x-icon lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
     </button>
   </div>
