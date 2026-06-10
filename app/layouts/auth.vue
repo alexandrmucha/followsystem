9e25baa99cbfd97d5ec0c246ae1c9e4799d79ccc
Alexandr Mucha <130845337+alexandrmucha@users.noreply.github.com>
2026-06-10 20:35:45 +0200

    reworked auth layout

--- app/layouts/auth.vue
+++ app/layouts/auth.vue
@@ -1,24 +1,11 @@
 <template>
-  <div class="min-h-screen bg-neutral-50 dark:bg-neutral-950 text-neutral-900 dark:text-neutral-100 flex flex-col">
+  <div class="min-h-screen flex flex-col bg-neutral-50 dark:bg-neutral-950 text-neutral-900 dark:text-neutral-100">
     <LayoutAnnouncements />
+    <LandingNavbar />
 
-    <div class="px-4 sm:px-6 flex flex-col sm:my-auto py-12">
-      <div class="w-full max-w-md mx-auto mb-5">
-        <NuxtLink to="/" class="text-sm text-neutral-400 dark:text-neutral-500 hover:text-neutral-600 dark:hover:text-neutral-300 transition-colors flex items-center space-x-2 mb-3">
-          <LucideArrowLeft :size="16" />
-          <span>{{ $t('auth.common.back_home') }}</span>
-        </NuxtLink>
-
-        <UiLogo />
-      </div>
-
-      <div class="flex flex-col items-center">
-        <LayoutAlerts />
-
-        <slot />
-      </div>
-        
-     </div>
-     
+    <div class="flex-1 flex flex-col items-center justify-center px-4 py-16">
+      <LayoutAlerts />
+      <slot />
+    </div>
   </div>
 </template>
