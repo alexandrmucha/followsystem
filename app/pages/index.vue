bfa1dd54cfadd57efbe5356a9019f53689331783
Alexandr Mucha <130845337+alexandrmucha@users.noreply.github.com>
2026-06-11 17:25:43 +0200

    added og image

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -11,14 +11,18 @@
 definePageMeta({ layout: 'landing' })
 
 const { t } = useI18n()
+const siteUrl = useRequestURL().origin
+
 useHead({ titleTemplate: null, title: t('landing.page_title') })
 useSeoMeta({
   description: t('landing.meta_description'),
   ogTitle: t('landing.page_title'),
   ogDescription: t('landing.meta_description'),
   ogType: 'website',
+  ogImage: `${siteUrl}/img/og_image.png`,
   twitterCard: 'summary_large_image',
   twitterTitle: t('landing.page_title'),
   twitterDescription: t('landing.meta_description'),
+  twitterImage: `${siteUrl}/img/og_image.png`,
 })
 </script>
